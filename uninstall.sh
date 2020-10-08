a70d1bf6a0fe63b8509417bf3bc87e2d114d9ee7
Alex Schittko <alex4108@live.com>
2020-10-08 12:36:02 -0500

    Refactor

--- uninstall.sh
+++ uninstall.sh
@@ -4,7 +4,7 @@ pidof systemd && systemd="true" || systemd="false"
 
 rm -rf /usr/bin/spot-reaper.sh
 rm -rf /tmp/spot-reaper/pid
-if [[ "${systemd}" == "true" ]]; 
+if [[ "${systemd}" == "true" ]]; then 
     rm -rf /etc/systemd/system/multi-user.target.wants/spot-reaper.service
 else
     rm -rf /etc/init.d/spot-reaper
